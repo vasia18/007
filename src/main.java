23d73722dc7727320c71a9c1360914cd49654bed
vasia18 <studiovasia18@gmail.com>
2021-01-26 23:19:10 +0300

    test

---
+++ src/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        short sirina;
+        short visota;
+        short dlinna;
+
+        Scanner in =new Scanner(System.in);
+
+        System.out.print("Введите длинну - ");  
+
+        System.out.print("Введите ширину - ");
+
+    }
+}
---
+++ src/ploshadPokraski.java
@@ -0,0 +1,11 @@
+public class ploshadPokraski {
+    short sirina;
+    short visota;
+    short dlinna;
+
+
+    public void ploshadSteni() {
+        short ploshad = (short) ((short) 2 *(dlinna+sirina) * visota);
+        System.out.println(ploshad);
+    }
+}
